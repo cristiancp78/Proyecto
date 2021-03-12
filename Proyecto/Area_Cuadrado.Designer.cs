3dc3e163b89c1bcaa6821fe5d7e3cfcf394f68d7
CRISTIAN CAMILO CASTRO PENA <ccamilocastro@ucundinamarca.edu.co>
2021-03-12 15:15:05 -0500

    Realizacion del metodo 2

--- Proyecto/Area_Cuadrado.Designer.cs
+++ Proyecto/Area_Cuadrado.Designer.cs
@@ -29,12 +29,140 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar Datos ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese la base del cuadrado ";
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(15, 97);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(144, 20);
+            this.txtBase.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingrese la altura del cuadrado";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(204, 97);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(144, 20);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "El area del cuadrado es ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(156, 190);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(119, 20);
+            this.txtArea.TabIndex = 6;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(196, 138);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(79, 26);
+            this.btnIngresar.TabIndex = 7;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(126, 235);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 8;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(98, 140);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 9;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Area_Cuadrado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(378, 308);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Area_Cuadrado";
             this.Text = "Area_Cuadrado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnNuevo;
     }
 }
--- Proyecto/Area_Cuadrado.cs
+++ Proyecto/Area_Cuadrado.cs
@@ -16,5 +16,61 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Form1 volver = new Form1();
+            volver.Visible = true;
+            this.Visible = false;
+        }
+
+        
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Vacio())
+                {
+                    int base_cuadrado = Convert.ToInt32(txtBase.Text);
+                    int altura = Convert.ToInt32(txtAltura.Text);
+                    int area = base_cuadrado * altura;
+
+                    txtArea.Text = "" + area;
+
+                }
+                else
+                {
+                    MessageBox.Show("Faltan datos por llenar ");
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        public bool Vacio()
+        {
+            if (txtBase.Text != "" && txtAltura.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtBase.Clear();
+            txtAltura.Clear();
+            txtArea.Clear();
+        }
     }
 }
