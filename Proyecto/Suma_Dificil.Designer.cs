6884be678a16949776572044985161d4b86d9025
CRISTIAN CAMILO CASTRO PENA <ccamilocastro@ucundinamarca.edu.co>
2021-03-13 13:01:21 -0500

    Creacion del modulo 6

--- Proyecto/Suma_Dificil.Designer.cs
+++ Proyecto/Suma_Dificil.Designer.cs
@@ -29,12 +29,131 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.labelSuma = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar dato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar los numeros";
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(134, 66);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeros.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "La suma de los numero es";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(58, 104);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(169, 104);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // labelSuma
+            // 
+            this.labelSuma.AutoSize = true;
+            this.labelSuma.Location = new System.Drawing.Point(171, 153);
+            this.labelSuma.Name = "labelSuma";
+            this.labelSuma.Size = new System.Drawing.Size(0, 13);
+            this.labelSuma.TabIndex = 6;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(58, 194);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 7;
+            this.btnVolver.Text = "Volver Menu";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(169, 194);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 8;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Suma_Dificil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(347, 244);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.labelSuma);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Suma_Dificil";
             this.Text = "Suma_Dificil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label labelSuma;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
--- Proyecto/Suma_Dificil.cs
+++ Proyecto/Suma_Dificil.cs
@@ -16,5 +16,65 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            Form1 volver = new Form1();
+            volver.Visible = true;
+            this.Visible = false;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtNumeros.Text != "")
+                {
+                    string numeros = txtNumeros.Text;
+                    float suma=0;
+
+                    while (numeros.Contains("  ")) //recorre toda la cadena de numeros y devuleve apenas encrente mas de 2 espacios
+                    {
+                        numeros = numeros.Replace("  ", " ");//remplaza o elimina los espacios cuando son mas de 2 y deja solo 1 
+                    }
+
+                    string[] valores = numeros.Split(' ');//Almaceno la cadena en un array separandolos al momento que se encuentre un espacio
+
+                    
+
+                    float[] Valores_numeros = new float[valores.Length];
+                    for (int i = 0; i < valores.Length; i++)
+                    {
+                        Valores_numeros[i] = float.Parse(valores[i]);
+                    }
+
+                    for(int x=0; x < Valores_numeros.Length; x++)
+                    {
+                        suma = suma + Valores_numeros[x];
+                    }
+
+                    labelSuma.Text = "" + suma;
+                }
+                else
+                {
+                    MessageBox.Show("Falta escribir los numeros");
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtNumeros.Clear();
+            labelSuma.Text = "";
+        }
     }
 }
