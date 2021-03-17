56f8ae8271e9d5f720279f91d8b9863d015313f3
DANILO ARTURO VARON SILVA <davaron@ucundinamarca.edu.co>
2021-03-17 11:55:37 -0500

    Se edito el form del modulo 5, creando la interfaz, el código respectivo para su funcionamiento, diseño del fondo y de los textos

--- Proyecto/Pangramas.Designer.cs
+++ Proyecto/Pangramas.Designer.cs
@@ -29,12 +29,158 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Verificar = new System.Windows.Forms.Button();
+            this.tb_PrimeraF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_SegundaF = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_PrimeraF = new System.Windows.Forms.Label();
+            this.lb_SegundaF = new System.Windows.Forms.Label();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.btn_VolverM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese la primera frase";
+            // 
+            // btn_Verificar
+            // 
+            this.btn_Verificar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Verificar.Location = new System.Drawing.Point(220, 217);
+            this.btn_Verificar.Name = "btn_Verificar";
+            this.btn_Verificar.Size = new System.Drawing.Size(79, 30);
+            this.btn_Verificar.TabIndex = 1;
+            this.btn_Verificar.Text = "Verificar";
+            this.btn_Verificar.UseVisualStyleBackColor = true;
+            this.btn_Verificar.Click += new System.EventHandler(this.btn_Verificar_Click);
+            // 
+            // tb_PrimeraF
+            // 
+            this.tb_PrimeraF.Location = new System.Drawing.Point(277, 108);
+            this.tb_PrimeraF.Name = "tb_PrimeraF";
+            this.tb_PrimeraF.Size = new System.Drawing.Size(192, 20);
+            this.tb_PrimeraF.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingrese la segunda frase";
+            // 
+            // tb_SegundaF
+            // 
+            this.tb_SegundaF.Location = new System.Drawing.Point(277, 157);
+            this.tb_SegundaF.Name = "tb_SegundaF";
+            this.tb_SegundaF.Size = new System.Drawing.Size(192, 20);
+            this.tb_SegundaF.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(216, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Pangramas";
+            // 
+            // lb_PrimeraF
+            // 
+            this.lb_PrimeraF.AutoSize = true;
+            this.lb_PrimeraF.BackColor = System.Drawing.Color.Transparent;
+            this.lb_PrimeraF.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_PrimeraF.ForeColor = System.Drawing.Color.White;
+            this.lb_PrimeraF.Location = new System.Drawing.Point(488, 110);
+            this.lb_PrimeraF.Name = "lb_PrimeraF";
+            this.lb_PrimeraF.Size = new System.Drawing.Size(0, 18);
+            this.lb_PrimeraF.TabIndex = 6;
+            // 
+            // lb_SegundaF
+            // 
+            this.lb_SegundaF.AutoSize = true;
+            this.lb_SegundaF.BackColor = System.Drawing.Color.Transparent;
+            this.lb_SegundaF.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_SegundaF.ForeColor = System.Drawing.Color.White;
+            this.lb_SegundaF.Location = new System.Drawing.Point(488, 159);
+            this.lb_SegundaF.Name = "lb_SegundaF";
+            this.lb_SegundaF.Size = new System.Drawing.Size(0, 18);
+            this.lb_SegundaF.TabIndex = 7;
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cerrar.Location = new System.Drawing.Point(476, 255);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(75, 44);
+            this.btn_Cerrar.TabIndex = 8;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = true;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // btn_VolverM
+            // 
+            this.btn_VolverM.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_VolverM.Location = new System.Drawing.Point(394, 255);
+            this.btn_VolverM.Name = "btn_VolverM";
+            this.btn_VolverM.Size = new System.Drawing.Size(76, 44);
+            this.btn_VolverM.TabIndex = 9;
+            this.btn_VolverM.Text = "Volver al Menu";
+            this.btn_VolverM.UseVisualStyleBackColor = true;
+            this.btn_VolverM.Click += new System.EventHandler(this.btn_VolverM_Click);
+            // 
+            // Pangramas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Proyecto.Properties.Resources.fondo_6;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(562, 311);
+            this.Controls.Add(this.btn_VolverM);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.lb_SegundaF);
+            this.Controls.Add(this.lb_PrimeraF);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_SegundaF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_PrimeraF);
+            this.Controls.Add(this.btn_Verificar);
+            this.Controls.Add(this.label1);
+            this.Name = "Pangramas";
             this.Text = "Pangramas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Verificar;
+        private System.Windows.Forms.TextBox tb_PrimeraF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_SegundaF;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_PrimeraF;
+        private System.Windows.Forms.Label lb_SegundaF;
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.Button btn_VolverM;
     }
 }
--- Proyecto/Pangramas.cs
+++ Proyecto/Pangramas.cs
@@ -16,5 +16,57 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void btn_Verificar_Click(object sender, EventArgs e)
+        {
+            String[] array = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"
+            , "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+            String pPalabra = tb_PrimeraF.Text;
+            String sPalabra = tb_SegundaF.Text;
+            bool verificarP; 
+            bool verificarS;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+               verificarP = pPalabra.ToLower().Contains(array[i]);
+
+                if (verificarP)
+                {
+                    lb_PrimeraF.Text = "SI";
+
+                }
+                else
+                {
+                    lb_PrimeraF.Text = "NO";
+
+                }
+
+                verificarS = sPalabra.ToLower().Contains(array[i]);
+
+                if (verificarS)
+                {
+
+                    lb_SegundaF.Text = "SI";
+                }
+                else
+                {
+
+                    lb_SegundaF.Text = "NO";
+                }
+            }
+
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_VolverM_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 F = new Form1();
+            F.Show();
+        }
     }
 }
