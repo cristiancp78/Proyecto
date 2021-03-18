78fae508c60f910309ab0fd8da90ab2808f4a6a7
CRISTIAN CAMILO CASTRO PENA <ccamilocastro@ucundinamarca.edu.co>
2021-03-17 20:38:51 -0500

    Modificacion de diseño del menu

--- Proyecto/Form1.Designer.cs
+++ Proyecto/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnM1 = new System.Windows.Forms.Button();
             this.btnM2 = new System.Windows.Forms.Button();
             this.btnM3 = new System.Windows.Forms.Button();
@@ -41,68 +42,118 @@ namespace Proyecto
             // 
             // btnM1
             // 
+            this.btnM1.BackColor = System.Drawing.Color.Transparent;
+            this.btnM1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM1.BackgroundImage")));
+            this.btnM1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM1.FlatAppearance.BorderSize = 0;
+            this.btnM1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM1.ForeColor = System.Drawing.SystemColors.Info;
             this.btnM1.Location = new System.Drawing.Point(37, 57);
             this.btnM1.Name = "btnM1";
             this.btnM1.Size = new System.Drawing.Size(89, 55);
             this.btnM1.TabIndex = 0;
             this.btnM1.Text = "Modulo 1- Mayor de dos numeros";
-            this.btnM1.UseVisualStyleBackColor = true;
+            this.btnM1.UseVisualStyleBackColor = false;
             this.btnM1.Click += new System.EventHandler(this.btnM1_Click);
             // 
             // btnM2
             // 
-            this.btnM2.Location = new System.Drawing.Point(157, 57);
+            this.btnM2.BackColor = System.Drawing.Color.Transparent;
+            this.btnM2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM2.BackgroundImage")));
+            this.btnM2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM2.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM2.FlatAppearance.BorderSize = 0;
+            this.btnM2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM2.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnM2.Location = new System.Drawing.Point(168, 57);
             this.btnM2.Name = "btnM2";
             this.btnM2.Size = new System.Drawing.Size(87, 55);
             this.btnM2.TabIndex = 1;
             this.btnM2.Text = "Modulo 2- Calcular area cuadrado";
-            this.btnM2.UseVisualStyleBackColor = true;
+            this.btnM2.UseVisualStyleBackColor = false;
             this.btnM2.Click += new System.EventHandler(this.btnM2_Click);
             // 
             // btnM3
             // 
-            this.btnM3.Location = new System.Drawing.Point(289, 57);
+            this.btnM3.BackColor = System.Drawing.Color.Transparent;
+            this.btnM3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM3.BackgroundImage")));
+            this.btnM3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM3.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM3.FlatAppearance.BorderSize = 0;
+            this.btnM3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM3.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnM3.Location = new System.Drawing.Point(295, 57);
             this.btnM3.Name = "btnM3";
             this.btnM3.Size = new System.Drawing.Size(79, 55);
             this.btnM3.TabIndex = 2;
             this.btnM3.Text = "Modulo 3- Cuenta letras";
-            this.btnM3.UseVisualStyleBackColor = true;
+            this.btnM3.UseVisualStyleBackColor = false;
             this.btnM3.Click += new System.EventHandler(this.btnM3_Click);
             // 
             // btnM4
             // 
+            this.btnM4.BackColor = System.Drawing.Color.Transparent;
+            this.btnM4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM4.BackgroundImage")));
+            this.btnM4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM4.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM4.FlatAppearance.BorderSize = 0;
+            this.btnM4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM4.ForeColor = System.Drawing.SystemColors.Info;
             this.btnM4.Location = new System.Drawing.Point(37, 154);
             this.btnM4.Name = "btnM4";
             this.btnM4.Size = new System.Drawing.Size(89, 47);
             this.btnM4.TabIndex = 3;
             this.btnM4.Text = "Modulo 4- Invertir palabra";
-            this.btnM4.UseVisualStyleBackColor = true;
+            this.btnM4.UseVisualStyleBackColor = false;
             this.btnM4.Click += new System.EventHandler(this.btnM4_Click);
             // 
             // btnM5
             // 
-            this.btnM5.Location = new System.Drawing.Point(157, 154);
+            this.btnM5.BackColor = System.Drawing.Color.Transparent;
+            this.btnM5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM5.BackgroundImage")));
+            this.btnM5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM5.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM5.FlatAppearance.BorderSize = 0;
+            this.btnM5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM5.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnM5.Location = new System.Drawing.Point(168, 154);
             this.btnM5.Name = "btnM5";
             this.btnM5.Size = new System.Drawing.Size(87, 47);
             this.btnM5.TabIndex = 4;
             this.btnM5.Text = "Modulo 5- Pangramas";
-            this.btnM5.UseVisualStyleBackColor = true;
+            this.btnM5.UseVisualStyleBackColor = false;
             this.btnM5.Click += new System.EventHandler(this.btnM5_Click);
             // 
             // btnM6
             // 
-            this.btnM6.Location = new System.Drawing.Point(289, 154);
+            this.btnM6.BackColor = System.Drawing.Color.Transparent;
+            this.btnM6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnM6.BackgroundImage")));
+            this.btnM6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnM6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnM6.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btnM6.FlatAppearance.BorderSize = 0;
+            this.btnM6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnM6.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnM6.Location = new System.Drawing.Point(295, 154);
             this.btnM6.Name = "btnM6";
             this.btnM6.Size = new System.Drawing.Size(79, 47);
             this.btnM6.TabIndex = 5;
             this.btnM6.Text = "Modulo 6- Una suma dificil";
-            this.btnM6.UseVisualStyleBackColor = true;
+            this.btnM6.UseVisualStyleBackColor = false;
             this.btnM6.Click += new System.EventHandler(this.btnM6_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label1.Location = new System.Drawing.Point(48, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(298, 17);
@@ -111,18 +162,28 @@ namespace Proyecto
             // 
             // btn_Cerrar
             // 
-            this.btn_Cerrar.Location = new System.Drawing.Point(157, 220);
+            this.btn_Cerrar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Cerrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Cerrar.BackgroundImage")));
+            this.btn_Cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Cerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Cerrar.FlatAppearance.BorderColor = System.Drawing.SystemColors.Window;
+            this.btn_Cerrar.FlatAppearance.BorderSize = 0;
+            this.btn_Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Cerrar.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_Cerrar.Location = new System.Drawing.Point(168, 220);
             this.btn_Cerrar.Name = "btn_Cerrar";
             this.btn_Cerrar.Size = new System.Drawing.Size(87, 26);
             this.btn_Cerrar.TabIndex = 7;
             this.btn_Cerrar.Text = "Cerrar";
-            this.btn_Cerrar.UseVisualStyleBackColor = true;
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
             this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(436, 268);
             this.Controls.Add(this.btn_Cerrar);
             this.Controls.Add(this.label1);
